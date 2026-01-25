0425daa369e2cffd105e8169e9ff99defec45463
Stephen Eaton <seaton@strobotics.com.au>
2026-01-25 15:11:43 +0800

    fix: add real SHA256 checksums for v1.1.2

--- Formula/lldap-cli.rb
+++ Formula/lldap-cli.rb
@@ -10,7 +10,7 @@ class LldapCli < Formula
   on_macos do
     on_arm do
       url "https://github.com/madeinoz67/lldap-cli/releases/download/v#{version}/lldap-cli-darwin-arm64"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
+      sha256 "66979a88fbde9d2486269a9e9659b5cd94d77c39618650d8072103843401c7cc"
 
       def install
         bin.install "lldap-cli-darwin-arm64" => "lldap-cli"
@@ -19,7 +19,7 @@ class LldapCli < Formula
 
     on_intel do
       url "https://github.com/madeinoz67/lldap-cli/releases/download/v#{version}/lldap-cli-darwin-x64"
-      sha256 "PLACEHOLDER_SHA256_DARWIN_X64"
+      sha256 "9ce4ad719a3b41ad98792d4c081cefbbe48b0443ff98465e5ef6b36359e7b4da"
 
       def install
         bin.install "lldap-cli-darwin-x64" => "lldap-cli"
@@ -30,7 +30,7 @@ class LldapCli < Formula
   on_linux do
     on_arm do
       url "https://github.com/madeinoz67/lldap-cli/releases/download/v#{version}/lldap-cli-linux-arm64"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "db5810056fd6b26549e0347c189c135db08731e0ff54fa14fc535a55f6a4845e"
 
       def install
         bin.install "lldap-cli-linux-arm64" => "lldap-cli"
@@ -39,7 +39,7 @@ class LldapCli < Formula
 
     on_intel do
       url "https://github.com/madeinoz67/lldap-cli/releases/download/v#{version}/lldap-cli-linux-x64"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X64"
+      sha256 "bf8348f184820be44e0e1fc13fd1e715081d844e2ec2cd2d44f7842b8d1eec0f"
 
       def install
         bin.install "lldap-cli-linux-x64" => "lldap-cli"
